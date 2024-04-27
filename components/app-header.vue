2222d5fab7d79afa158483340fdf2804c13a8bfd
Тавабилов Денис Ринатович <d.tavabilov@prosoftsystems.ru>
2024-04-27 08:53:51 +0500

    end 56 lessons

---
+++ components/app-header.vue
@@ -0,0 +1,14 @@
+<template>
+  <header class="flex justify-between items-center mt-10">
+    <NuxtLink to="/" class="text-xl font-bold"> Finance Tracker </NuxtLink>
+    <div>
+      <template>
+        <UAvatar
+          size="sm"
+          src="https://avatars.githubusercontent.com/u/739984?v=4"
+          alt="Avatar"
+        />
+      </template>
+    </div>
+  </header>
+</template>
